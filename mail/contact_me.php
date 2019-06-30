e305012a4d75caa08de056ef36e4e8e2a32c7d27
Martina <rozzel03@email.franklin.edo>
2019-06-30 18:01:03 -0400

    email updates

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,7 +11,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "mrozzell@mgail.com"; //
+$to = "mrozzell@gmail.com"; //
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@ygmail.com\n"; /
